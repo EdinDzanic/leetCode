0d105d4dc5ea861652aa0f37d597e8d4775506c9
Edin Dzanic <edin.dz@hotmail.com>
2018-04-16 13:16:23 +0200

    Add solution for 657

---
+++ 657. Judge Route Circle/Solution.java
@@ -0,0 +1,26 @@
+class Solution {
+    public boolean judgeCircle(String moves) {
+        int x = 0, y = 0;
+        
+        for(char move: moves.toCharArray()) {
+            switch(move){
+                case 'R':
+                    x++;
+                    break;
+                case 'L':
+                    x--;
+                    break;
+                case 'U':
+                    y++;
+                    break;
+                case 'D':
+                    y--;
+                    break;
+                default:
+                    break;
+            }
+        }
+        
+        return x == 0 && y == 0;
+    }
+}
