718cb4aece6c3caa7ab1782011f79498d2c099b8
Edin Dzanic <edin.dz@hotmail.com>
2018-10-08 10:10:36 +0200

    Add solution for 62

---
+++ 62. Unique Paths/Solution.java
@@ -0,0 +1,21 @@
+class Solution {
+    public int uniquePaths(int m, int n) {
+        int[][] grid = new int[n][];
+        
+        for (int i = 0; i < n; i++)
+            grid[i] = new int[m];
+        
+        grid[0][0] = 1;
+        
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < m; j++) {
+                if (i > 0)
+                    grid[i][j] += grid[i - 1][j];
+                if (j > 0)
+                    grid[i][j] += grid[i][j - 1];
+            }
+        }
+
+        return grid[n - 1][m - 1];
+    }
+}
