76a17e6e01294233d21c85f8245781149ca43672
Edin Dzanic <edin.dz@hotmail.com>
2018-10-23 11:52:30 +0200

    Add solution for 53

---
+++ 53. Maximum Subarray/Solution.java
@@ -0,0 +1,18 @@
+class Solution {
+    public int maxSubArray(int[] nums) {
+        if (nums.length == 0) return 0;
+        
+        int max = nums[0];
+        int curMax = nums[0];
+        
+        for (int i = 1; i < nums.length; i++) {
+            
+            curMax = Math.max(curMax + nums[i], nums[i]);
+            
+            if(curMax > max)
+                max = curMax;
+        }
+        
+        return max;
+    }
+}
